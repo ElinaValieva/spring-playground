1ba5becd663d64b260c85d30ec241c8004a161af
ElinaValieva <veaufa@mail.ru>
2018-10-07 12:52:19 +0300

    feat: image-service
    add mvc model with test values on initial

--- image-service/src/main/java/com/elina/image/ImageController.java
+++ image-service/src/main/java/com/elina/image/ImageController.java
@@ -1,7 +1,12 @@
 package com.elina.image;
 
+import com.elina.image.model.Image;
+import com.elina.image.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * author: ElinaValieva on 07.10.2018
@@ -9,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ImageController {
 
+    @Autowired
+    private ImageService imageService;
+
     @GetMapping("/images")
-    public String images() {
-        return "images server, say hi";
+    public List<Image> images() {
+        return imageService.getImages();
     }
 }
