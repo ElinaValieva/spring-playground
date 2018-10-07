78ee2ae40a9562ee2e7dc1230355dfcfda001b8f
ElinaValieva <veaufa@mail.ru>
2018-10-07 14:01:06 +0300

    fix(gallery-service): rest api

--- gallery-service/src/main/java/com/elina/gallery/GalleryController.java
+++ gallery-service/src/main/java/com/elina/gallery/GalleryController.java
@@ -3,14 +3,25 @@ package com.elina.gallery;
 import com.elina.gallery.model.Gallery;
 import com.elina.gallery.service.GalleryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * author: ElinaValieva on 07.10.2018
  */
 @RestController
+@RequestMapping("/")
 public class GalleryController {
+
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello from Gallery Service running at port: " + environment.getProperty("local.server.port");
+    }
 
     @Autowired
     private GalleryService galleryService;
@@ -18,5 +29,10 @@ public class GalleryController {
     @GetMapping("/gallery")
     public Gallery galleries() {
         return galleryService.getGallery();
+    }
+
+    @GetMapping("/hello")
+    public String hey() {
+        return "hello, beach!";
     }
 }
