6264b21623f9e6d572d704cddcce59e86a16bf06
ElinaValieva <veaufa@mail.ru>
2018-10-07 12:53:12 +0300

    feat: gallery-service
    add mvc model which uploading data from image-service

--- gallery-service/src/main/java/com/elina/gallery/GalleryController.java
+++ gallery-service/src/main/java/com/elina/gallery/GalleryController.java
@@ -1,5 +1,8 @@
 package com.elina.gallery;
 
+import com.elina.gallery.model.Gallery;
+import com.elina.gallery.service.GalleryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GalleryController {
 
+    @Autowired
+    private GalleryService galleryService;
+
     @GetMapping("/gallery")
-    public String galleries() {
-        return "gallery service, say hello!";
+    public Gallery galleries() {
+        return galleryService.getGallery();
     }
 }
---
+++ gallery-service/src/main/java/com/elina/gallery/service/GalleryService.java
@@ -0,0 +1,26 @@
+package com.elina.gallery.service;
+
+import com.elina.gallery.model.Gallery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * author: ElinaValieva on 07.10.2018
+ */
+@Service
+public class GalleryService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Gallery getGallery(){
+        Gallery gallery = new Gallery();
+        gallery.setId(1);
+        List<Object> images = restTemplate.getForObject("http://image-service/images/", List.class);
+        gallery.setImages(images);
+        return gallery;
+    }
+}
