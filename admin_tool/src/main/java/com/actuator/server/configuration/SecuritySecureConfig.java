d5c4333411936e2cd621852a1f9326695d9d5ef2
ElinaValieva <veaufa@mail.ru>
2019-11-14 18:11:35 +0300

    Fix: security, rename main class

--- admin_tool/src/main/java/com/actuator/server/configuration/SecuritySecureConfig.java
+++ admin_tool/src/main/java/com/actuator/server/configuration/SecuritySecureConfig.java
@@ -1,32 +1,24 @@
-package com.actuator.server;
+package com.actuator.server.configuration;
 
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
-@EnableAdminServer
-@SpringBootApplication
-public class AdminToolApplication {
+@Configuration
+public class SecuritySecureConfig {
 
     private final String adminContextPath;
 
-    public AdminToolApplication(AdminServerProperties adminServerProperties) {
+    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
         this.adminContextPath = adminServerProperties.getContextPath();
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(AdminToolApplication.class, args);
     }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChainSecure(ServerHttpSecurity http) {
-
         return http.authorizeExchange()
                 .pathMatchers(adminContextPath + "/assets/**").permitAll()
                 .pathMatchers(adminContextPath + "/login").permitAll()
