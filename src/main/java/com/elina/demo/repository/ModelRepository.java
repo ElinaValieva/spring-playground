317997088d63624d83e9b32b7cc574bbf7850adc
ElinaValieva <veaufa@mail.ru>
2020-01-19 23:20:38 +0300

    Refactor: remove unused

--- src/main/java/com/elina/demo/repository/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elina.demo.repository;
-
-
-import com.elina.demo.model.Model;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ModelRepository extends CrudRepository<Model, Long> {
-
-}
