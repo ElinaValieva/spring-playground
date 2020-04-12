6dc9c466c68a8b84943b7b445a829eb47f69068c
ElinaValieva <veaufa@mail.ru>
2020-04-12 10:25:10 +0300

    Fix: rewrite with streams

--- spring-annotation/src/main/java/com/example/annotation/rand/RandGeneratorBeanPostProcessor.java
+++ spring-annotation/src/main/java/com/example/annotation/rand/RandGeneratorBeanPostProcessor.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ReflectionUtils;
 
-import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Random;
 
 @Configuration
@@ -13,15 +13,15 @@ public class RandGeneratorBeanPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        Field[] declaredFields = bean.getClass().getDeclaredFields();
-        for (Field declaredField : declaredFields) {
-            if (declaredField.isAnnotationPresent(RandGenerator.class)) {
-                RandGenerator annotation = declaredField.getAnnotation(RandGenerator.class);
-                declaredField.setAccessible(true);
-                int value = new Random().nextInt(annotation.max() - annotation.min()) + annotation.min();
-                ReflectionUtils.setField(declaredField, bean, value);
-            }
-        }
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(RandGenerator.class))
+                .forEach(field -> {
+                    RandGenerator annotation = field.getAnnotation(RandGenerator.class);
+                    field.setAccessible(true);
+                    int value = new Random().nextInt(annotation.max() - annotation.min()) + annotation.min();
+                    ReflectionUtils.setField(field, bean, value);
+                });
+
         return bean;
     }
 
