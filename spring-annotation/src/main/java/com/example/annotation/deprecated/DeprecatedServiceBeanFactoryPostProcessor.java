3ab36230831729b061a2acd8fef6a67e7b3d9980
ElinaValieva <veaufa@mail.ru>
2020-04-12 10:13:43 +0300

    Fix: getting configuration bean class name from reader

--- spring-annotation/src/main/java/com/example/annotation/deprecated/DeprecatedServiceBeanFactoryPostProcessor.java
+++ spring-annotation/src/main/java/com/example/annotation/deprecated/DeprecatedServiceBeanFactoryPostProcessor.java
@@ -4,7 +4,10 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.type.MethodMetadata;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Field;
 
 @Service
 public class DeprecatedServiceBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
@@ -17,8 +20,7 @@ public class DeprecatedServiceBeanFactoryPostProcessor implements BeanFactoryPos
             String beanClassName = beanDefinition.getBeanClassName();
 
             if (beanClassName == null)
-                return;
-
+                beanClassName = resolveBeanClassNameFromConfigurationBeanDefinition(beanDefinition);
             try {
                 Class<?> aClass = Class.forName(beanClassName);
                 DeprecatedService annotation = aClass.getAnnotation(DeprecatedService.class);
@@ -28,6 +30,18 @@ public class DeprecatedServiceBeanFactoryPostProcessor implements BeanFactoryPos
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private String resolveBeanClassNameFromConfigurationBeanDefinition(BeanDefinition beanDefinition) {
+        try {
+            Object reader = Class.forName("org.springframework.context.annotation.ConfigurationClassBeanDefinitionReader$ConfigurationClassBeanDefinition")
+                    .cast(beanDefinition);
+            Field field = reader.getClass().getDeclaredField("factoryMethodMetadata");
+            field.setAccessible(true);
+            return ((MethodMetadata) field.get(reader)).getReturnTypeName();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
         }
     }
 }
